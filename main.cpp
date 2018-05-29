5e4acb7fc5cad114757ed1d523f0e2d9f150206a
dj <dj_jxc@163.com>
2018-05-29 18:53:52 +0800

    test

--- main.cpp
+++ main.cpp
@@ -18,10 +18,12 @@ int main()
 
 	//int is_cross = ab_cross_cd(p11, p12, p21, p22);
 	//type_cross(is_cross);
-	
-	Point p1(10.0, 20.0), p2(15.0, 10.0);
+									
+	Point p1(10.0, 20.0), p2(15.0, 10.0), p3(13.0, 10.0), p4(20.0, 20.0);
 	Line l1(p1, p2);
 	l1.showLine();
+	int type = l1.isLine_cross(p3, p4);
+	l1.type_cross(type);
 	cout << "x:" << p1.getX() << endl;
 	cout << "y:" << p1.getY() << endl;
 
